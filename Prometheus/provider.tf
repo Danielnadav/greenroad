a3323c518558e39add553f2c983787dda8c9861d
nadav daniel <>
2023-07-31 11:45:20 +0300

    changed provider

--- Prometheus/provider.tf
+++ Prometheus/provider.tf
@@ -6,7 +6,7 @@ provider "helm" {
 }
 
 # Used to interact with the resources supported by Kubernetes.
-# The provider needs to be configured with the proper credentials before it can be used.
+
 provider "kubernetes" {
   config_path = pathexpand(var.kube_config)
 }
